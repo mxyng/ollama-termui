0122123aad120b95b3f76943910a2eb20f9510e2
Michael Yang <mxyng@pm.me>
2025-01-29 18:49:29 -0800

    move help to own file

---
+++ chat/help.go
@@ -0,0 +1,55 @@
+package chat
+
+import (
+	"github.com/charmbracelet/bubbles/v2/help"
+	"github.com/charmbracelet/bubbles/v2/key"
+	bbt "github.com/charmbracelet/bubbletea/v2"
+	"github.com/charmbracelet/lipgloss/v2"
+)
+
+type Help struct {
+	help.Model
+	Help        key.Binding
+	ClearScreen key.Binding
+	PageUp      key.Binding
+	PageDown    key.Binding
+	Cancel      key.Binding
+	Suspend     key.Binding
+	Quit        key.Binding
+
+	showCancel bool
+}
+
+func (m Help) Update(msg bbt.Msg) (_ Help, cmd bbt.Cmd) {
+	switch msg := msg.(type) {
+	case bbt.WindowSizeMsg:
+		m.Width = msg.Width
+	}
+
+	m.Model, cmd = m.Model.Update(msg)
+	return m, cmd
+}
+
+func (m Help) View() string {
+	return m.Model.View(m)
+}
+
+func (m Help) ShortHelp() []key.Binding {
+	return []key.Binding{m.Help}
+}
+
+func (m Help) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{m.ClearScreen},
+		{m.PageUp, m.PageDown},
+		{m.Help, m.Cancel, m.Quit},
+	}
+}
+
+func (m Help) Height() int {
+	if m.ShowAll {
+		return lipgloss.Height(m.FullHelpView(m.FullHelp()))
+	}
+
+	return lipgloss.Height(m.ShortHelpView(m.ShortHelp()))
+}
---
+++ chat/message.go
@@ -0,0 +1,140 @@
+package chat
+
+import (
+	"io"
+	"iter"
+	"strings"
+
+	"github.com/charmbracelet/glamour"
+	"github.com/charmbracelet/lipgloss/v2"
+)
+
+type chatMsg struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+
+	roleStyle lipgloss.Style
+}
+
+var (
+	defaultUserRoleStyle      = lipgloss.NewStyle().SetString("User: ").Bold(true)
+	defaultAssistantRoleStyle = lipgloss.NewStyle().SetString("Assistant: ").Bold(true)
+)
+
+func (m chatMsg) Render(contentRenderer *glamour.TermRenderer) string {
+	content, err := contentRenderer.Render(m.Content)
+	if err != nil {
+		content = m.Content
+	}
+
+	return m.roleStyle.Render() + content
+}
+
+type chatResponseMsg struct {
+	io.ReadCloser
+}
+
+type exchange struct {
+	User, Assistant     chatMsg
+	Complete, Cancelled bool
+}
+
+type Chat struct {
+	msgs []exchange
+
+	width, height int
+	history       History
+	sb            strings.Builder
+}
+
+func (m *Chat) SetWidth(w int) {
+	m.width = w
+	m.build()
+}
+
+func (m *Chat) SetHeight(h int) {
+	m.height = h
+	m.build()
+}
+
+func (m Chat) Height() int {
+	return min(m.height, lipgloss.Height(m.sb.String()))
+}
+
+func (m *Chat) Add(s string) {
+	m.msgs = append(m.msgs, exchange{
+		User: chatMsg{
+			Role:      "user",
+			Content:   s,
+			roleStyle: defaultUserRoleStyle,
+		},
+		Assistant: chatMsg{
+			Role:      "assistant",
+			roleStyle: defaultAssistantRoleStyle,
+		},
+	})
+
+	m.build()
+
+	m.history.Push(s)
+}
+
+func (m *Chat) Update(s string) {
+	m.msgs[len(m.msgs)-1].Assistant.Content += s
+	m.build()
+}
+
+func (m *Chat) Complete() {
+	if len(m.msgs) > 0 {
+		m.msgs[len(m.msgs)-1].Complete = true
+	}
+}
+
+func (m *Chat) Cancel() {
+	if len(m.msgs) > 0 && !m.msgs[len(m.msgs)-1].Complete {
+		m.msgs[len(m.msgs)-1].Cancelled = true
+	}
+}
+
+func (m *Chat) Reset() {
+	m.msgs = nil
+	m.build()
+}
+
+func (m *Chat) build() {
+	contentRenderer, _ := glamour.NewTermRenderer(
+		glamour.WithEnvironmentConfig(),
+		glamour.WithAutoStyle(),
+		glamour.WithWordWrap(m.width-4),
+	)
+
+	m.sb.Reset()
+	for _, msg := range m.msgs {
+		m.sb.WriteString(msg.User.Render(contentRenderer))
+		if msg.Assistant.Content != "" {
+			m.sb.WriteString(msg.Assistant.Render(contentRenderer))
+		}
+	}
+}
+
+func (m Chat) String() string {
+	return m.sb.String()
+}
+
+func (m Chat) Messages() iter.Seq[chatMsg] {
+	return func(yield func(chatMsg) bool) {
+		for _, msg := range m.msgs {
+			if msg.Cancelled {
+				continue
+			}
+
+			if !yield(msg.User) {
+				break
+			} else if msg.Assistant.Content != "" {
+				if !yield(msg.Assistant) {
+					break
+				}
+			}
+		}
+	}
+}
